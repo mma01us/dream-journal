da3c21802bf08b3945df5cc19a2be40a61293e44
Michael Ma <mm10516@nyu.edu>
2022-05-28 13:35:35 -0400

    Update db.js

--- db.js
+++ db.js
@@ -29,6 +29,4 @@ Dream.plugin(URLSlugs('name'));
 
 mongoose.model('User', User);
 mongoose.model('Dream', Dream);
-// unfortunately this is hardcoded for now
 mongoose.connect(`${prefix}://${host}/${name}`);
-//mongoose.connect(`mongodb+srv://michael:HhcoE6KDAQ5cr6YL@dreamjournalcluster.f9nai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
